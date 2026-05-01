a9f9723280527b1a264703ab1cc309ccf4fe45da
denis123-botg <denisturkov97@gmail.com>
2026-05-01 15:17:32 +0200

    Update main.py
    Signed-off-by: denis123-botg <denisturkov97@gmail.com>

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ import discord
 import os
 from discord.ext import commands
 from discord.ui import View, Button
+from aiohttp import web
+import asyncio
 
-# Берем токен из секретов Render
 TOKEN = os.getenv('BOT_TOKEN')
-
 URL_SAYTA = "https://denis123-botg.github.io/sirion_forms/"
 ADMIN_CHANNEL_ID = 1216754939616039014
 ROLE_ID = 1259828977942528111
+
+# --- МИНИ-СЕРВЕР ДЛЯ RENDER ---
+async def handle(request):
+    return web.Response(text="Bot is alive")
+
+async def run_server():
+    app = web.Application()
+    app.router.add_get('/', handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.getenv("PORT", 10000))
+    site = web.TCPSite(runner, '0.0.0.0', port)
+    await site.start()
+# ------------------------------
 
 class AdminAction(View):
     def __init__(self, uid):
@@ -20,6 +34,8 @@ class AdminAction(View):
         if m:
             await m.add_roles(inter.guild.get_role(ROLE_ID))
             await inter.response.send_message(f"✅ Игрок <@{self.uid}> принят!", ephemeral=True)
+        else:
+            await inter.response.send_message("❌ Игрок не найден", ephemeral=True)
     @discord.ui.button(label="Отказать ❌", style=discord.ButtonStyle.red)
     async def no(self, inter, btn):
         await inter.response.send_message("❌ Отказано", ephemeral=True)
@@ -35,7 +51,8 @@ class PersistentView(View):
 
 class MyBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="!", intents=discord.Intents.all())
+        intents = discord.Intents.all()
+        super().__init__(command_prefix="!", intents=intents)
     async def setup_hook(self):
         self.add_view(PersistentView())
 
@@ -56,8 +73,13 @@ async def on_message(msg):
         except: pass
     await bot.process_commands(msg)
 
-if TOKEN:
-    bot.run(TOKEN)
-else:
-    print("ОШИБКА: Токен не найден в Environment Variables!")
-    
+async def main():
+    asyncio.create_task(run_server())
+    async with bot:
+        await bot.start(TOKEN)
+
+if __name__ == "__main__":
+    if TOKEN:
+        asyncio.run(main())
+    else:
+        print("TOKEN NOT FOUND")
